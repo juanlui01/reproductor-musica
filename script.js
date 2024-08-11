427fabcf9f5e94d5434e215d344944a289ff1f4e
Juan Luis <juannluissbarriossgarciaa@gmail.com>
2024-08-12 00:38:18 +0200

    modifique el index.html

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 
 
-
 // let cancion = {
 //     _id: "Abba" ,
 //     title: "Waterloo Deluxe-Edition",
@@ -20,13 +19,38 @@
 // MiFuncion (5)
 console.log(axios)
 
+//To do: Implementar get de listado de canciones
+// axios.get('https://leonardoapi.onrender.com/songs')
+//     .then(function (response) {
+//         // handle success
+//         response.data.songs.map((song) => {
+//            
+ //const element = buildSongHtml(song)
 
-axios.get('https://leonardoapi.onrender.com/songs')
-    .then(function (response) {
-        // handle success
-        response.data.songs.map((song) => {
-            let div = document.createElement('div')
-            div.setAttribute('class', "flex hover:bg-slate-100 transition-all cursor-pointer py-3 px-1 gap-5 items-center")
+//             document.getElementById('container').appendChild(element)
+
+//             //div.innerHTML =`
+//                // <div class="Titulo">
+    
+//                     //<h1 class="text-3xl font-bold color-blue">
+//                    //  SoundFast
+//                     //</h1>
+//                 //</div>
+            
+//             //`
+
+//             //document.getElementById ('header').appendChild(div)
+
+
+
+//         })
+        
+        
+//     })
+
+function buildSongHtml(song) {
+    let div = document.createElement('div')
+            div.setAttribute('class', "flex hover:bg-slate-100 transition-all cursor-pointer py-3 px-1 gap-5 items-center text-white ")
 
             div.innerHTML = `
                 <img src="${song.path.front}" alt="" class="h-16">
@@ -35,8 +59,121 @@ axios.get('https://leonardoapi.onrender.com/songs')
                     <p class="opacity-60">${song.author}</p>
                 </div>
             `
+            return div
+}
+
+function buildMainHtml(songs) {
+   songs.forEach(song => {
+    const element = buildSongHtml(song)
+
+    document.getElementById('container').appendChild(element)
+   }); 
+
+   
+}
+
+function getSongs() {
+    return axios.get('https://leonardoapi.onrender.com/songs')
+        .then(function (response) { return response.data })
+.catch( (error) => {
+    console.error("Ha ocurrido un error al solicitar las canciones", error)
+})
+}
 
 
-            document.getElementById('container').appendChild(div)
-        })
-    })
+
+
+const songs = [
+      {
+        path: {
+          audio: "https://leonardoapi.onrender.com/assets/music/audio/panicatthedisco_houseofmemories.mp3",
+          front: "https://leonardoapi.onrender.com/assets/music/img/panicatthedisco_houseofmemories.jpg"
+        },
+        _id: "63e54b2e7c38342067355300",
+        title: "House Of Memories",
+        album: "Death of a Bachelor",
+        duration: "3:30",
+        author: "Panic At The Disco"
+      },
+      {
+        path: {
+          audio: "https://leonardoapi.onrender.com/assets/music/audio/greenday_boulevardofbrokendreams.mp3",
+          front: "https://leonardoapi.onrender.com/assets/music/img/greenday_boulevardofbrokendreams.jpg"
+        },
+        _id: "63e5599b7c38342067355305",
+        title: "Boulevard of Broken Dreams",
+        album: "American Idiot",
+        author: "Green Day",
+        duration: "4:22"
+      },
+      {
+        path: {
+          audio: "https://leonardoapi.onrender.com/assets/music/audio/onerepublic_countingstars.mp3",
+          front: "https://leonardoapi.onrender.com/assets/music/img/onerepublic_countingstars.jpg"
+        },
+        _id: "63e55bf57c38342067355307",
+        title: "Counting Stars",
+        album: "Native",
+        author: "OneRepublic",
+        duration: "4:17"
+      },
+      {
+        path: {
+          audio: "https://leonardoapi.onrender.com/assets/music/audio/catherinefeeny_mrblue.mp3",
+          front: "https://leonardoapi.onrender.com/assets/music/img/catherinefeeny_mrblue.jpg"
+        },
+        _id: "63e545d97c383420673552f4",
+        title: "Mr Blue",
+        album: "Mr Blue",
+        duration: "3:03",
+        author: "Catherine Feeny"
+      },
+      {
+        path: {
+          audio: "https://leonardoapi.onrender.com/assets/music/audio/theweeknd_blindinglights.mp3",
+          front: "https://leonardoapi.onrender.com/assets/music/img/theweeknd_blindinglights.jpg"
+        },
+        _id: "63e55e8c7c3834206735530a",
+        title: "Blinding Lights",
+        album: "Blinding Lights",
+        author: "The Weeknd",
+        duration: "3:22"
+      },
+      {
+        path: {
+          audio: "https://leonardoapi.onrender.com/assets/music/audio/inthevalleybelow_peaches.mp3",
+          front: "https://leonardoapi.onrender.com/assets/music/img/inthevalleybelow_peaches.jpg"
+        },
+        _id: "63e549827c383420673552fc",
+        title: "Peaches",
+        album: "The Belt",
+        duration: "4:47",
+        author: "In The Valley Below"
+      },
+      {
+        path: {
+          audio: "https://leonardoapi.onrender.com/assets/music/audio/oliviarodrigo_favoritecrime.mp3",
+          front: "https://leonardoapi.onrender.com/assets/music/img/oliviarodrigo_favoritecrime.jpg"
+        },
+        _id: "63f446d7feccc52c021c729b",
+        title: "Favorite Crime",
+        album: "Sour",
+        author: "Olivia Rodrigo",
+        duration: "3:05"
+      },
+      {
+        path: {
+          audio: "https://leonardoapi.onrender.com/assets/music/audio/thesmiths_panic.mp3",
+          front: "https://leonardoapi.onrender.com/assets/music/img/thesmiths_panic.jpg"
+        },
+        _id: "63e55d467c38342067355308",
+        title: "Panic",
+        album: "The World Won't Listen",
+        author: "The Smiths",
+        duration: "2:22"
+      }
+      
+    ]
+  
+    buildMainHtml(songs)    
+    getSongs()
---
+++ songs.js
@@ -0,0 +1,169 @@
+export default{   
+    "songs": [
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/panicatthedisco_houseofmemories.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/panicatthedisco_houseofmemories.jpg"
+            },
+            "_id": "63e54b2e7c38342067355300",
+            "title": "House Of Memories",
+            "album": "Death of a Bachelor",
+            "duration": "3:30",
+            "author": "Panic At The Disco"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/greenday_boulevardofbrokendreams.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/greenday_boulevardofbrokendreams.jpg"
+            },
+            "_id": "63e5599b7c38342067355305",
+            "title": "Boulevard of Broken Dreams",
+            "album": "American Idiot",
+            "author": "Green Day",
+            "duration": "4:22"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/onerepublic_countingstars.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/onerepublic_countingstars.jpg"
+            },
+            "_id": "63e55bf57c38342067355307",
+            "title": "Counting Stars",
+            "album": "Native",
+            "author": "OneRepublic",
+            "duration": "4:17"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/catherinefeeny_mrblue.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/catherinefeeny_mrblue.jpg"
+            },
+            "_id": "63e545d97c383420673552f4",
+            "title": "Mr Blue",
+            "album": "Mr Blue",
+            "duration": "3:03",
+            "author": "Catherine Feeny"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/theweeknd_blindinglights.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/theweeknd_blindinglights.jpg"
+            },
+            "_id": "63e55e8c7c3834206735530a",
+            "title": "Blinding Lights",
+            "album": "Blinding Lights",
+            "author": "The Weeknd",
+            "duration": "3:22"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/inthevalleybelow_peaches.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/inthevalleybelow_peaches.jpg"
+            },
+            "_id": "63e549827c383420673552fc",
+            "title": "Peaches",
+            "album": "The Belt",
+            "duration": "4:47",
+            "author": "In The Valley Below"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/oliviarodrigo_favoritecrime.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/oliviarodrigo_favoritecrime.jpg"
+            },
+            "_id": "63f446d7feccc52c021c729b",
+            "title": "Favorite Crime",
+            "album": "Sour",
+            "author": "Olivia Rodrigo",
+            "duration": "3:05"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/thesmiths_panic.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/thesmiths_panic.jpg"
+            },
+            "_id": "63e55d467c38342067355308",
+            "title": "Panic",
+            "album": "The World Won't Listen",
+            "author": "The Smiths",
+            "duration": "2:22"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/dancroll_complimentyoursoul.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/dancroll_complimentyoursoul.jpg"
+            },
+            "_id": "63e557047c38342067355302",
+            "album": "Sweet Disarray",
+            "author": "Dan Croll",
+            "title": "Compliment Your Soul",
+            "duration": "2:58"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/theroyalconcept_goldrushed.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/theroyalconcept_goldrushed.jpg"
+            },
+            "_id": "63e558a87c38342067355303",
+            "title": "Goldrushed",
+            "author": "The Royal Concept",
+            "album": "Goldrushed",
+            "duration": "3:47"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/jcole_sheknows.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/jcole_sheknows.jpg"
+            },
+            "_id": "63e54aa77c383420673552ff",
+            "author": "J. Cole",
+            "title": "She Knows",
+            "album": "Born Sinner",
+            "duration": "4:08"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/hozier_takemetochurch.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/hozier_takemetochurch.jpg"
+            },
+            "_id": "63e55b447c38342067355306",
+            "title": "Take Me To Church",
+            "album": "Take Me To Church E.P.",
+            "author": "Hozier",
+            "duration": "4:01"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/daftpunk_getlucky.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/daftpunk_getlucky.jpg"
+            },
+            "_id": "63e548fa7c383420673552fa",
+            "title": "Get Lucky",
+            "album": "Get Lucky",
+            "duration": "4:06",
+            "author": "Daft Punk"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/bedroom_inmyhead.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/bedroom_inmyhead.jpg"
+            },
+            "_id": "63e556737c38342067355301",
+            "author": "Bedroom",
+            "title": "In my Head",
+            "album": "In my Head",
+            "duration": "6:00"
+        },
+        {
+            "path": {
+                "audio": "https://leonardoapi.onrender.com/assets/music/audio/theneighbourhood_softcore.mp3",
+                "front": "https://leonardoapi.onrender.com/assets/music/img/theneighbourhood_softcore.jpg"
+            },
+            "_id": "63e55de87c38342067355309",
+            "title": "Softcore",
+            "album": "The Neighbourhood",
+            "author": "The Neighbourhood",
+            "duration": "3:31"
+        }
+    ]
+}
